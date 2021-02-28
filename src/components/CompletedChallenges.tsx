49cbac4f91356278ec28606a8971eda395c909c5
alexsandrogms <alexsandrogmsdev@gmail.com>
2021-02-28 15:09:18 -0300

    added alarm clock gif, remove react lottie

--- src/components/CompletedChallenges.tsx
+++ src/components/CompletedChallenges.tsx
@@ -8,7 +8,7 @@ export function CompletedChallenges() {
   return (
     <div className={styles.completedChallengeContainer}>
       <span>Desafios completos</span>
-      <span>{challengesCompleted}</span>
+      <span>{challengesCompleted || 0}</span>
     </div>
   );
 }
